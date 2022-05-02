8e3c7e58b9e467e4fd87374c9077793ec10a7cf9
MagdielAndres <magdiel.vampire@gmail.com>
2022-05-02 10:44:58 -0500

    Agregando ESLint y Arreglando la dentación a los archivos de nuestro proyecto

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,27 +1,27 @@
-const FizzbuzzService = require('./../../lib/services/FizzbuzzService');
+const FizzbuzzService = require("./../../lib/services/FizzbuzzService");
 
-describe('Test FizzBuzz Service', () => { 
-    test('Test triker= 1', () => { 
-        const explorer1 = {name: "Explorer1", score: 1}        
+describe("Test FizzBuzz Service", () => { 
+    test("Test triker= 1", () => { 
+        const explorer1 = {name: "Explorer1", score: 1};        
         const explorerTrickScore = FizzbuzzService.ApplyValidationInExplorer(explorer1);     
         expect(explorerTrickScore).toEqual({name: "Explorer1", score: 1, trick: 1});
     });
 
-    test('Cuando el score sea divisible entre 3 ,tricke: FIZZ ', () => { 
-        const explorer3 = {name: "Explorer3", score: 3}
-        const explorerTrickeFizz = FizzbuzzService.ApplyValidationInExplorer(explorer3)
+    test("Cuando el score sea divisible entre 3 ,tricke: FIZZ ", () => { 
+        const explorer3 = {name: "Explorer3", score: 3};
+        const explorerTrickeFizz = FizzbuzzService.ApplyValidationInExplorer(explorer3);
 
         expect(explorerTrickeFizz).toEqual({name: "Explorer3", score: 3, trick: "FIZZ"});
     });
 
-    test('Cuando el score sea divisible sea entre 5, trick: BUZZ ', () => { 
-        const explorer5 = {name: "Explorer5", score: 5}
-        const explorerTrickBuzz = FizzbuzzService.ApplyValidationInExplorer(explorer5)
+    test("Cuando el score sea divisible sea entre 5, trick: BUZZ ", () => { 
+        const explorer5 = {name: "Explorer5", score: 5};
+        const explorerTrickBuzz = FizzbuzzService.ApplyValidationInExplorer(explorer5);
 
         expect(explorerTrickBuzz).toEqual({name: "Explorer5", score: 5, trick: "BUZZ"});
     });
 
-    test('Cuando el score sea divisible sea entr 3 y 5, trick: FIZZBUZZ', () => { 
+    test("Cuando el score sea divisible sea entr 3 y 5, trick: FIZZBUZZ", () => { 
         const explorer15 = {name: "Explorer15", score: 15};
         const explorerTrickFizzbuzz = FizzbuzzService.ApplyValidationInExplorer(explorer15);
 
